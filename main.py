7a97712c0e34d0d1d198cf06a6678c27c11a4432
Gudmundur Páll Kjartansson - synced from SCIENCE AD <fnt359@tuxray-1.diku.dk>
2013-02-19 15:28:37 +0100

    main.py now asks for y instead of Y

--- main.py
+++ main.py
@@ -6,20 +6,19 @@ import matplotlib.pyplot as plt
 if __name__ == "__main__":
     image_format = "png" 
 
-    if (raw_input("Compute question 1.1: Plot different gaussians? (Y/n): ") == "Y"):
+    if (raw_input("Compute question 1.1: Plot different gaussians? (y/n): ") == "y"):
         plt.figure()
         est.gaussian_plots()
         plt.close()
         print "... Saved result as image gaussian.%s" % img_format
-
     print 
 
-    if (raw_input("Compute question 1.2: Random data set? (no will skip 2-7) (Y/n): ") == "Y"):
+    if (raw_input("Compute question 1.2: Random data set? (no will skip 2-7) (y/n): ") == "y"):
         data = est.data(100)
         print data
 	print
     
-        if(raw_input("Compute question 1.3: Maximum likelihood? (Y/n)") == "Y"):
+        if(raw_input("Compute question 1.3: Maximum likelihood? (y/n)") == "y"):
             plt.figure()
             (sample_mean, sample_cov) = est.estimate_params(data)
             print "Sample mean:"
@@ -30,28 +29,28 @@ if __name__ == "__main__":
             plt.close()
         print
     
-        if(raw_input("Compute question 1.5: histogram based probability estimate? (Y/n): ") == "Y"):
+        if(raw_input("Compute question 1.5: histogram based probability estimate? (y/n): ") == "y"):
             plt.figure()
             est.histogram_plot(data, 10)
             print "Saved results as histogram1.png and histogram2.%s" % img_format
             plt.close()
         print
     
-        if(raw_input("Compute question 1.6: Histogram together with analytical solution? (Y/n): ") == "Y"):
+        if(raw_input("Compute question 1.6: Histogram together with analytical solution? (y/n): ") == "y"):
             plt.figure()
             est.histogram_and_analytical_plot(data,10)
             plt.close()
             print "Saved results as hist_and_analytical.%s" % img_format
         print
     
-        if(raw_input("Compute question 1.7: 2-dimensional histogram? (Y/n): ") == "Y"):
+        if(raw_input("Compute question 1.7: 2-dimensional histogram? (Y/n): ") == "y"):
             plt.figure()
             est.histogram_plot_2d(data)
             plt.close()
             print "Saved results as histogram3d_10bins.png, histogram3d_15bins.png, histogram3d_20bins.%s" % img_format
     print
 
-    if(raw_input("Compute question 1.8: Monte Carlo? (Y/n): ") == "Y"):
+    if(raw_input("Compute question 1.8: Monte Carlo? (Y/n): ") == "y"):
         plt.figure()
         sam.convergence_plot(0.5)
 	sam.convergence_log_plot(0.5)
@@ -62,7 +61,7 @@ if __name__ == "__main__":
 
     import objdetection as obj
     import os.path
-    if(raw_input("Compute question 1.9: Visualise kande1 probability density") == "Y"):
+    if(raw_input("Compute question 1.9: Visualise kande1 probability density? (y/n): ") == "y"):
         plt.figure()
 
         if not os.path.exists("data"):
@@ -76,7 +75,7 @@ if __name__ == "__main__":
         plt.close()
         print
 
-    if(raw_input("Compute question 1.10: Plot object postition and spread of kande1? (Y/n): ") == "Y"):
+    if(raw_input("Compute question 1.10: Plot object postition and spread of kande1? (y/n): ") == "y"):
         plt.figure()
 
 	obj.contour_plot("kande1.pnm")
@@ -84,7 +83,7 @@ if __name__ == "__main__":
         plt.close()
         print
 
-    if(raw_input("Compute question 1.11: Visualise kande2 probability density") == "Y"):
+    if(raw_input("Compute question 1.11: Visualise kande2 probability density? (y/n): ") == "y"):
         plt.figure()
 
         if not os.path.exists("data"):
@@ -99,7 +98,7 @@ if __name__ == "__main__":
         plt.close()
         print
 
-    if(raw_input("Compute question 1.11: Plot object postition and spread of kande2? (Y/n): ") == "Y"):
+    if(raw_input("Compute question 1.11: Plot object postition and spread of kande2? (y/n): ") == "y"):
         plt.figure()
 	obj.contour_plot("kande2.pnm")
         print "Saved results as kande2_and_contours.png"
